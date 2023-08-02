8772ae21553dd3d2e0841f0f6d1a9c2ddfdd9324
Delight Sheriff <delightsheriff@gmail.com>
2023-08-02 15:15:13 +0100

    usestate

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,18 @@
+import { useState } from "react";
+
 const Home = () => {
+    const [name, setName] = useState('mario');
+    const [age, setAge] = useState(25);
+
     const handleClick = ()=>{
-        console.log("hello ninjas");
-    }
-    const handleClickAgain = (name)=>{
-        console.log("hello " + name);
+        setName("Philip")
+        setAge(70);
     }
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{name} is {age} years old</p>
             <button onClick={handleClick}>Click me</button>
-            <button onClick={()=>{handleClickAgain('Delight')}}>Click me again</button>
         </div>
      );
 }
