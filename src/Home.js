c762f297e5b52e350c32e2a2895833e53f08b724
Delight Sheriff <delightsheriff@gmail.com>
2023-08-02 16:35:06 +0100

    use effect dependecies

--- src/Home.js
+++ src/Home.js
@@ -7,19 +7,23 @@ const Home = () => {
         {title: 'Welcome party', body: 'lorem ipsum...', author: 'Philips', id:2},
         {title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id:3}
     ]);
+
+    const [name, setName] = useState('mario');
     
     const handleDelete = (id)=>{
         const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs)
+        setBlogs(newBlogs);
     }
 
     useEffect(()=>{
         console.log('use effect ran');
-        console.log(blogs);
-    });
+        console.log(name);
+    }, [name]);
     return ( 
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+            <button onClick={()=> setName("Delight")}>Change Name</button>
+            <p>{name}</p>
             {/* <BlogList blogs={blogs.filter((blog)=> blog.author === 'mario')} title="Marios Blogs!" /> */}
         </div>
      );
