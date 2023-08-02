e0b2664f7029996a594fc79dddfaa589b4b34e50
Delight Sheriff <delightsheriff@gmail.com>
2023-08-02 14:45:53 +0100

    multiple components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const title = "Welcome to the new blog";
-  const likes = 50;
-  const person = {name: 'delight', age: 30};
-  const link = "http://www.google.com"
-
 
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{title}</h1>
-        <p>Liked {likes} times</p>
-        <p>{10}</p>
-        <p>{"hello, delight"}</p>
-
-        <a href={link}>Google site</a>
+        <Home />
       </div>
     </div>
   );
