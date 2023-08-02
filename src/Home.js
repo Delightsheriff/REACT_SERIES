b23d3ac97470f9f38f5aed6d00c02ffbb29a7db1
Delight Sheriff <delightsheriff@gmail.com>
2023-08-02 16:17:13 +0100

    use effect

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -12,10 +12,15 @@ const Home = () => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs)
     }
+
+    useEffect(()=>{
+        console.log('use effect ran');
+        console.log(blogs);
+    });
     return ( 
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
-            <BlogList blogs={blogs.filter((blog)=> blog.author === 'mario')} title="Marios Blogs!" />
+            {/* <BlogList blogs={blogs.filter((blog)=> blog.author === 'mario')} title="Marios Blogs!" /> */}
         </div>
      );
 }
